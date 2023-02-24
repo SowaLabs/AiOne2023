e35dce097a5692d5f3214b3d74c029786fdba118
Gregor Mohorko <grega@mohorko.info>
2023-02-24 15:54:11 +0100

    feat: Take Brain data from folder where app is running

--- AiOneChatbot/Program.cs
+++ AiOneChatbot/Program.cs
@@ -34,7 +34,7 @@ public class Program
 		services.AddScoped<SpeechGenerator>();
 		services.AddSingleton(new Brain(
 			"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",
-			@"C:\Repositories\Playing and Testing\Hackathon\AiOne2023\FAQ_embeddings.jsonl"
+			$"{AppContext.BaseDirectory}/FAQ_embeddings.jsonl"
 		));
 		services.AddSingleton<LipSyncGenerator>();
 		services.AddSingleton<TextAnswerGenerator>();
