9eb892501cc9de5b0172f52c10053d3606bb1299
mIHA <miha@sowalabs.com>
2023-02-26 09:45:44 +0100

    Adds language detector. Adds German speech synthesis.

--- AiOneChatbot/Application/Chatbot/Dto/ChatbotResponse.cs
+++ AiOneChatbot/Application/Chatbot/Dto/ChatbotResponse.cs
@@ -1,10 +1,14 @@
-﻿namespace AiOneChatbot.Application.Chatbot.Dto
+﻿using System.Security.Policy;
+
+namespace AiOneChatbot.Application.Chatbot.Dto
 {
 	public class ChatbotResponse
 	{
 		public string AnswerText { get; init; }
 
 		public string Prompt { get; init; }
+
+		public string Language { get; init; }
 
 		/// <summary>
 		/// Base64 encoded.
